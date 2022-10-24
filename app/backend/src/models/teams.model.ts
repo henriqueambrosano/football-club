3129300ec354bb31621cb02272db0808d45bd02b
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-10-24 15:16:28 -0300

    criando model para teams

---
+++ app/backend/src/models/teams.model.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+export default class TeamsModel extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+TeamsModel.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING(30),
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
