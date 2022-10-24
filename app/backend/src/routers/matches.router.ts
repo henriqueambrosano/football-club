50735afaf3207bcfcb3bfca9d6ad32588bc9c8b1
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-10-24 20:47:13 -0300

    modificacoes no match router para poder utilizar outros models no service de match + adicao da rota post

--- app/backend/src/routers/matches.router.ts
+++ app/backend/src/routers/matches.router.ts
@@ -2,11 +2,17 @@ import { Router } from 'express';
 import MatchesController from '../controllers/matches.controller';
 import MatchesService from '../services/matches.service';
 import MatchesModel from '../models/matches.model';
+import UserModel from '../models/user.model';
+import TeamsModel from '../models/teams.model';
 
 const MatchesRouter = Router();
 
-const matchesController = new MatchesController(new MatchesService(MatchesModel));
+const matchesController = new MatchesController(new MatchesService(
+  MatchesModel,
+  UserModel,
+  TeamsModel,
+));
 
 MatchesRouter.get('/matches', (req, res) => matchesController.getAll(req, res));
-
+MatchesRouter.post('/matches', (req, res) => matchesController.saveMatch(req, res));
 export default MatchesRouter;
