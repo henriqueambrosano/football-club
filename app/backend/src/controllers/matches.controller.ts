fb888b4d77aa5582597f78588b78b46da78195af
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-10-24 20:47:47 -0300

    funcao para salvar partida no controller de match

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -20,4 +20,25 @@ export default class MatchesController {
       res.status(500).json({ message: 'Internal server error' });
     }
   }
+
+  async saveMatch(req: Request, res: Response) {
+    try {
+      const { homeTeam, awayTeam } = req.body;
+      const { authorization } = req.headers;
+      if (homeTeam === awayTeam) {
+        return res.status(422)
+          .json({ message: 'It is not possible to create a match with two equal teams' });
+      }
+      const validateHome = await this.matchesService.findTeamById(homeTeam);
+      const validateAway = await this.matchesService.findTeamById(awayTeam);
+      console.log(validateAway);
+      if (!validateHome || !validateAway) {
+        return res.status(404).json({ message: 'There is no team with such id!' });
+      }
+      const newMatch = await this.matchesService.createMatch(req.body, authorization);
+      res.status(201).json(newMatch);
+    } catch (error) {
+      res.status(401).json({ message: 'Token must be a valid token' });
+    }
+  }
 }
