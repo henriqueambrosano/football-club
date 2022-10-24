8fd3b41ef9f276983f1780c7849e291b63977037
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-10-24 20:47:35 -0300

    novas funcoes no service de match

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,8 +1,15 @@
 import TeamsModel from '../models/teams.model';
 import MatchesModel from '../models/matches.model';
+import IMatch from '../interfaces/IMatch';
+import UserModel from '../models/user.model';
+import TokenAuth from '../auth/tokenAuth';
 
 export default class MatchesService {
-  constructor(private matchesModel: typeof MatchesModel) { }
+  constructor(
+    private matchesModel: typeof MatchesModel,
+    private userModel: typeof UserModel,
+    private teamsModel: typeof TeamsModel,
+  ) { }
 
   async getAll(): Promise <MatchesModel[]> {
     const matches = await this.matchesModel.findAll({
@@ -21,5 +28,19 @@ export default class MatchesService {
         { model: TeamsModel, as: 'teamAway', attributes: ['teamName'] },
       ] });
     return matches;
+  }
+
+  async findTeamById(id: number) {
+    const match = await this.teamsModel.findOne({ where: { id } });
+    return match;
+  }
+
+  async createMatch(match: IMatch, authorization: string | undefined) {
+    if (!authorization) throw new Error();
+    const { data } = TokenAuth.decrypt(authorization);
+    const user = await this.userModel.findOne({ where: { password: data.password } });
+    if (!user) throw new Error();
+    const newMatch = await this.matchesModel.create({ ...match, inProgress: true });
+    return newMatch;
   }
 }
