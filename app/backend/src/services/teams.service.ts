4ec9eee1d71aef5d42de4df6a65cd98435867ec0
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-10-24 16:11:13 -0300

    adicionando tipagem no retorno das funcoes do service de teams

--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -3,12 +3,12 @@ import TeamsModel from '../models/teams.model';
 export default class TeamsService {
   constructor(private teamsModel: typeof TeamsModel) { }
 
-  async getAll() {
+  async getAll(): Promise <TeamsModel[]> {
     const teams = await this.teamsModel.findAll();
     return teams;
   }
 
-  async getById(id: number) {
+  async getById(id: number): Promise <TeamsModel | null> {
     const team = await this.teamsModel.findOne({ where: { id } });
     return team;
   }
